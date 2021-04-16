5ae7d676a857ff7c460c8d5dadbe4bf71fb0328b
FrankvandenBoogaard <74182536+FrankvandenBoogaard@users.noreply.github.com>
2021-04-16 02:35:14 +0200

    End of section 8

--- src/features/events/eventDashboard/EventListItem.jsx
+++ src/features/events/eventDashboard/EventListItem.jsx
@@ -25,7 +25,7 @@ export default function EventListItem({ event }) {
       <Segment>
         <span>
           <Icon name='clock' /> {format(event.date, "MMMM d, yyyy H:mm")}
-          <Icon name='marker' /> {event.venue}
+          <Icon name='marker' /> {event.venue.address}
         </span>
       </Segment>
       <Segment secondary>
